dd533f37779fc65717ed9ec95715536a9796b780
Tadeu Tupinambá. <tadeu.tupiz@gmail.com>
2025-03-16 21:47:10 -0300

    small change

--- internal/core/usecase/dto/client_output.go
+++ internal/core/usecase/dto/client_output.go
@@ -3,9 +3,10 @@ package dto
 import "time"
 
 type ClientOutput struct {
-	ID        int       `json:"id"`
-	Name      string    `json:"name"`
-	CPF       string    `json:"cpf"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	CPF         string    `json:"cpf"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
+	Description string    `json:"description"`
 }
--- internal/core/usecase/mappers/client.go
+++ internal/core/usecase/mappers/client.go
@@ -1,16 +1,19 @@
 package mappers
 
 import (
+	"fmt"
+
 	"github.com/tupizz/restaurant-food-golang-api-fiap/internal/core/domain/entities"
 	"github.com/tupizz/restaurant-food-golang-api-fiap/internal/core/usecase/dto"
 )
 
 func ToClientDTO(client entities.Client) dto.ClientOutput {
 	return dto.ClientOutput{
-		ID:        client.ID,
-		Name:      client.Name,
-		CPF:       client.CPF,
-		CreatedAt: client.CreatedAt,
-		UpdatedAt: client.UpdatedAt,
+		ID:          client.ID,
+		Name:        client.Name,
+		CPF:         client.CPF,
+		CreatedAt:   client.CreatedAt,
+		UpdatedAt:   client.UpdatedAt,
+		Description: fmt.Sprintf("Cliente %s", client.Name),
 	}
 }
